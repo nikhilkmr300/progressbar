5b306e2320f05aa402736e6a47ba70841b314328
Nikhil Kumar <nikhilkmr300@gmail.com>
2021-06-22 05:51:18 +0530

    Style and color set using strings, added comments

---
+++ README.md
@@ -0,0 +1,9 @@
+# progressbar
+
+Progress bar utility for Bash/Zsh.
+
+While there exist third-party packages such as
+[`tqdm`](https://github.com/tqdm/tqdm) for progress bars in Python, I've found
+that every time I need one while shell scripting, I need to roll my own.
+
+## Features
--- progressbar.sh
+++ progressbar.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#! /usr/local/bin/bash
 
 # Here are a few sample progress and blank styles to choose from:
 # +--------------------+---------------------+
@@ -15,6 +15,20 @@
 
 set -e
 
+colorreset="\e[0m"
+
+#######################################
+# Converts name to ANSI escape code.
+# Globals:
+#   colorreset
+# Arguments:
+#   Name of color or ANSI escape code in the format "\e[32m"
+# Outputs:
+#   ANSI escape code to stdout
+# Examples:
+#   *) colormap "blue"
+#   *) colormap "\e[32m"
+#######################################
 function colormap() {
     case $1 in
         black)      echo "\e[30m" ;;
@@ -25,67 +39,184 @@ function colormap() {
         magenta)    echo "\e[35m" ;;
         cyan)       echo "\e[36m" ;;
         white)      echo "\e[37m" ;;
-        reset)      echo "\e[0m" ;;
+        "")         echo $colorreset ;;
+        reset)      echo $colorreset ;;
+        \\e*m)      echo $1 ;;
         *)          echo $1; echo "usage: colormap <color>" 1>&2
-                    echo "colors: black red green yellow blue magenta cyan white reset" 1>&2
                     exit -1 ;;
     esac
 }
 
-function setup() {
-    progstyle=$1; shift
-    blankstyle=$1; shift
+#######################################
+# Configures style from an input string.
+# Globals:
+#   styles
+# Arguments:
+#   String containing styles for completed progress, blanks, left edge and right
+#   edge, in that order, separated by colons ";". 
+#   For example, if you want a progress bar that looks like
+#       [>>>>>>>...] 70%
+#   pass the string as ">;.;[;]". If you do not want to set a parameter, leave 
+#   it blank, and it will use the default.
+# Outputs:
+#   None
+# Example:
+#   parsestyles ">;;[;]"
+#######################################
+function parsestyles() {
+    local delim=";"
+    local keys=(
+        "prog"
+        "blank"
+        "edgeleft"
+        "edgeright"
+    )
+    vals=($(echo $1 | sed "s/$delim/$delim /g"))
 
-    local edgestyle=$1; shift
-    if [ $edgestyle == "[" ]; then
-        leftedge="["
-        rightedge="]"
-    elif [ $edgestyle == "(" ]; then
-        leftedge="("
-        rightedge=")"
-    elif [ $edgestyle == "{" ]; then
-        leftedge="{"
-        rightedge="}"
-    else
-        leftedge=$edgestyle
-        rightedge=$edgestyle
-    fi
-
-    progcolor=$(colormap $1); shift
-    blankcolor=$(colormap $1); shift
-    edgecolor=$(colormap $1); shift
-    percentcolor=$(colormap $1); shift
-    reset=$(colormap reset)
-
-    disappear=$1; shift
-    bell=$1
+    declare -Ag styles;
+    local style;
+    for key in ${keys[@]}; do
+        style=$(echo ${vals[0]} | sed "s/$delim//g")
+        echo "style: $style"
+        if [[ $style != "" ]]; then
+            styles[$key]=$style
+        else
+            styles[$key]=" "
+        fi
+        vals=(${vals[@]:1})
+    done
 }
 
+#######################################
+# Configures colors from an input string.
+# Globals:
+#   colors
+# Arguments:
+#   String containing styles for completed progress, blanks, left edge and right
+#   edge, in that order, separated by colons ";". 
+#   For example, if you want a progress bar with green completed progress,
+#   blanks with color code "\e[32m", colorless edges, and blue percentage pass 
+#   the string as "green;\e[32m;reset;reset;blue" or "green;\e[32m;;;blue". If 
+#   you do not want to set a parameter, leave it blank, and it will use the 
+#   default.
+# Outputs:
+#   None
+#######################################
+function parsecolors() {
+    local delim=";"
+    local keys=(
+        "prog"
+        "blank"
+        "edge"
+        "percent"
+    )
+    vals=($(echo $1 | sed "s/$delim/$delim /g"))
+
+    declare -Ag colors;
+    local color;
+    for key in ${keys[@]}; do
+        color=$(echo ${vals[0]} | sed "s/$delim//g")
+        colors[$key]=$(colormap $color)
+        vals=(${vals[@]:1})
+    done
+}
+
+#######################################
+# Setup configuration.
+# Globals:
+#   styles      :   Style-related config, setup in `parsestyles`
+#   colors      :   Color-related config, setup in `parsecolors`
+#   disappear   :   If true, progress bar disappears after 100%
+#   alert       :   If true, rings bell ('\a') after 100%
+# Arguments:
+#   stylestring :   As described in `parsestyles`
+#   colorstring :   As described in `parsecolors`
+#   disappear   :   boolean
+#   alert       :   boolean
+# Outputs:
+#   None
+#######################################
+function setup() {
+    parsestyles "$1"; shift
+    parsecolors "$1"; shift
+    disappear=$1; shift
+    alert=$1
+}
+
+#######################################
+# Prints config to stdout in JSON format (for debugging).
+#######################################
+function printsetup() {
+    local key; local i;
+
+    echo "{"
+
+    echo -e "\t\"styles\": {"
+    i=1
+    for key in ${!styles[@]}; do
+        echo -ne "\t\t\"$key\": \"${styles[$key]}\""
+        if [ $i -ne ${#styles[@]} ]; then
+            echo ","
+        else
+            echo ""
+        fi
+        i=$(( i + 1 ))
+    done
+    echo -e "\t},"
+
+    echo -e "\t\"colors\": {"
+    i=1
+    for key in ${!colors[@]}; do
+        echo -ne "\t\t\"$key\": \"\\\\e${colors[$key]:2}\""
+        if [ $i -ne ${#colors[@]} ]; then
+            echo ","
+        else
+            echo ""
+        fi
+        i=$(( i + 1 ))
+    done
+    echo -e "\t}"
+
+    echo -e "\t\"disappear\": $disappear,"
+
+    echo -e "\t\"alert\": $alert"
+
+    echo "}"
+}
+
+#######################################
+# Main function to generate progress bar.
+# Arguments:
+#   percent     :   Current percentage completed
+#   width       :   Total width (columns) of progress bar on screen
+#######################################
 function main() {
-    tput civis
+    tput civis                                              # Hiding cursor
 
-    local -i percent=$1; shift
-    local -i width=$(( $1 - 4 )); shift
+    local -i percent=$1; shift                              # Percent completed
+    local -i width=$(( $1 - 4 )); shift                     # Number of columns in progress bar
 
-    local -i prog=$(( percent * width / 100 ))
-    local -i blanks=$(( width - prog ))
+    local -i prog=$(( percent * width / 100 ))              # Number of columns for completed
+    local -i blanks=$(( width - prog ))                     # Number of columns for remaining
 
-    printf "${edgecolor}${leftedge}${reset}"
+    # Printing progress.
+    printf "${colors[edge]}${styles[edgeleft]}${colorreset}"
     local i;
     for (( i=0; i<prog; i++ )); do
-        printf "${progcolor}${progstyle}${reset}"
+        printf "${colors[prog]}${styles[prog]}${colorreset}"
     done
+    # Printing remaining (blanks).
     for (( i=0; i<blanks; i++ )); do
-        printf "${blankcolor}${blankstyle}${reset}"
+        printf "${colors[blank]}${styles[blank]}${colorreset}"
     done
-    printf "${edgecolor}${rightedge}${reset}"
-    printf "${percentcolor}%4d%s${reset}" $percent "%"
+    printf "${colors[edge]}${styles[edgeright]}${colorreset}"
+    printf "${colors[percent]}%4d%s${colorreset}" $percent "%"
 
     if [ $percent -ne 100 ]; then
-        printf "\r"
+        printf "\r"                                         # Flush
     elif [ $percent -eq 100 ]; then
-        tput cvvis
-        if [ $bell == true ]; then
+        tput cvvis                                          # Unhiding cursor
+        if [ $alert == true ]; then
             echo -ne "\a"
         fi
         if [ $disappear == true ]; then
@@ -96,8 +227,9 @@ function main() {
     fi
 }
 
-setup "■" "□" "[" red green blue cyan false false
+setup "█; ;|;|" "\e[32m;green;;blue" false true
+printsetup
 
-for i in $(seq 0 100); do
-    main $i 80
+for i in $(seq 1 100); do
+    main i 80
 done
